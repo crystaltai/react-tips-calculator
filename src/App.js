78dbe2ebfbd3852888a41e92d2593b8f068debf0
Crystal Tai <crystaltai315@gmail.com>
2022-12-01 00:17:39 -0800

    feat: outer container of tip calculator

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import Container from '@mui/material/Container';
+import { Box, Container, Stack } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
-    <Container maxWidth='md'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <Container
+      maxWidth='md'
+      sx={{
+        bgcolor: '#E5E9F0',
+        height: '100vh',
+        padding: '100px',
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          minWidth: 400,
+          height: 700,
+          padding: '20px',
+          bgcolor: '#ffffff',
+          borderRadius: 3,
+        }}
+      >
+        <Stack spacing={2}>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+        </Stack>
+      </Box>
     </Container>
   );
 }
