271b41bf60c6eb5fd6e023827f895bd57a8ca749
Crystal Tai <crystaltai315@gmail.com>
2022-11-30 21:55:32 -0800

    initial commit and install dependencies

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Container from '@mui/material/Container';
+import './App.css';
+
+function App() {
+  return (
+    <Container maxWidth='md'>
+      <header className='App-header'>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </Container>
+  );
+}
+
+export default App;
